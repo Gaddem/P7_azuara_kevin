a2414c2555ba1ba6f5c2d113abea4175a2b379a3
lmk-kevin <kevin@lmksystem.fr>
2023-02-09 12:02:11 +0100

    Fix filters choice

--- src/actions/sort_search.js
+++ src/actions/sort_search.js
@@ -5,11 +5,8 @@ function checkerEveryIncludes (arrayToTest, stableArray){
 function fetchDataSort(arrayToFilter) {
    let sortIngr =  selectedIngredient?.length<1 ? arrayToFilter : arrayToFilter.filter((recipe)=>{
         let NameIngredients=recipe.ingredients.map((ingr)=>{return ingr.ingredient.toLowerCase()});
-        
-        let foundIngredientsToFiltre = NameIngredients.some(r=> selectedIngredient.indexOf(r) >= 0);
         let reCheck = checkerEveryIncludes(NameIngredients,selectedIngredient);
-        console.log("NameIngredients",NameIngredients,"selectedIngredient",selectedIngredient, " == ",reCheck);
-        if(foundIngredientsToFiltre===true && reCheck === true){
+        if(reCheck === true){
             return recipe;
         }   
    }).map((recipe)=>{
@@ -25,8 +22,9 @@ function fetchDataSort(arrayToFilter) {
     });
 
     let sortUstensils =  selectedUstensil?.length<1 ? sortAppareil : sortAppareil.filter((recipe)=>{
-        let foundUstensilsToFiltre = recipe.ustensils.map(v => v?.toLowerCase()).some(r=> selectedUstensil.indexOf(r) >= 0);
-        if(foundUstensilsToFiltre===true){
+        let foundUstensilsToFiltre = recipe.ustensils.map(v => v?.toLowerCase());
+        let resultCheck = checkerEveryIncludes(foundUstensilsToFiltre,selectedUstensil);
+        if(resultCheck===true ){
             return recipe;
         }   
     }).map((recipe)=>{
