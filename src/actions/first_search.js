40783af7325eea1fc856f0e9cbdb45044333719d
lmk-kevin <kevin@lmksystem.fr>
2024-02-02 18:17:43 +0100

    Loop native first search

--- src/actions/first_search.js
+++ src/actions/first_search.js
@@ -1,14 +1,32 @@
 function fetchDataSearch(valueSearch) {
+  //fonction qui permet de filtrer en fonction d'une valeur de recherche
   tempoArray = [];
   let lowerCaseSearch = valueSearch.toLowerCase();
-  tempoArray = recipes.filter(
-    (detailRecipe) =>
-      detailRecipe.name.toLowerCase().includes(lowerCaseSearch) ||
-      detailRecipe.description.toLowerCase().includes(lowerCaseSearch) ||
-      detailRecipe.ingredients.filter((ingr) =>
-        ingr.ingredient.toLowerCase().includes(lowerCaseSearch)
-      ).length > 0
-  );
+  for (let index = 0; index < recipes.length; index++) {
+    const recipe = recipes[index];
+    let lowerCaseNameRecipe = recipe.name.toLowerCase();
+    let lowerCaseDescRecipe = recipe.description.toLowerCase();
+    if (lowerCaseNameRecipe.includes(lowerCaseSearch)) {
+      //condition sur le titre de la recette
+      tempoArray.push(recipe);
+    } else if (lowerCaseDescRecipe.includes(lowerCaseSearch)) {
+      //condition surla description de la recette
+      tempoArray.push(recipe);
+    } else {
+      //condition sur la liste des ingrédients de la recette
+      for (
+        let indexIngr = 0;
+        indexIngr < recipe.ingredients.length;
+        indexIngr++
+      ) {
+        const ingr = recipe.ingredients[indexIngr];
+        let lowerCaseNameIngr = ingr.ingredient.toLowerCase();
+        if (lowerCaseNameIngr.includes(lowerCaseSearch)) {
+          tempoArray.push(recipe);
+        }
+      }
+    }
+  }
   return tempoArray;
 }
 
